820b831dfef0b74392fd146d23dadb1e60c790d7
Jeyanth <66862827+jeyanth-jr@users.noreply.github.com>
2023-04-07 10:51:17 +0530

    Update login.php

--- login.php
+++ login.php
@@ -7,8 +7,9 @@
 </head>
 
 <body>
+    <h1>Welcome</h1>
     <form action="" method="POST">
-        <input type="text" name="username" placeholder="username" required>
+        <input type="text" name="username" placeholder="enter roll no" required>
         <input type="password" name="password" placeholder="password" required>
         <input type="submit" name="submit" value="submit">
     </form>
@@ -24,13 +25,14 @@
         }
         $username = mysqli_real_escape_string($conn, $_POST['username']);
         $password = mysqli_real_escape_string($conn, $_POST['password']);
-        $query = "SELECT * FROM login_data WHERE username='$username'";
+        $query = "SELECT * FROM login_data WHERE roll_no='$username'";
         $result = mysqli_query($conn, $query);
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
             if ($password === $row['password']) {
                 session_start();
                 $_SESSION['username'] = $username;
+                $_SESSION['usertype'] = $row['usertype'];
                 if ($row['usertype'] == 'admin') {
                     header("Location: http://localhost/test/admin.php");
                     exit();
@@ -48,6 +50,17 @@
     }
     ?>
     <p>Not a member? <a href="register.php">Sign up now</a></p>
+    <footer>
+        <p>Created with <span class="heart-icon">&hearts;</span> by Jeyanth:</p>
+        <a href="https://github.com/jeyanth-jr"><img src="https://img.icons8.com/color/48/000000/github--v1.png"
+                alt="github" /></a>
+        <a href="https://www.linkedin.com/in/jeyanth-v-643895194/"><img
+                src="https://img.icons8.com/color/48/000000/linkedin.png" alt="linkedin" /></a>
+        <a href="https://www.instagram.com/jeyanth__jr/"><img
+                src="https://img.icons8.com/color/48/000000/instagram-new.png" alt="instagram" /></a>
+
+    </footer>
+
 </body>
 
 </html>
